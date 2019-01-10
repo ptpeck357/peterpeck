7f825f0cf2c26ad20395a023059c9c0e3d93d94c
Peter Peck <karatekid4christ@gmail.com>
2019-01-09 19:58:01 -0800

    Added jQuery Portfolio

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
 
-    $('.crossfade').crossfade({
-        backgroundPosition: 'center top'
-    });
+  // Crossfade
+  $('.crossfade').crossfade({
+    backgroundPosition: 'center top'
+  });
 
-    $('.crossfade-small').crossfade({
-        backgroundPosition: 'center center'
-    });
+  $('.crossfade-small').crossfade({
+    backgroundPosition: 'center center'
+  });
 
-    ScrollReveal().reveal('.reveal',
+  //SCroll Reveal
+  ScrollReveal().reveal('.reveal',
     {
-        duration: 1000
+      duration: 1000
     });
+
+  //jQuery Portfolio
+  $(".grid").portfolio({
+    cols: 3, // Number of columns you want your thumbnails to take. Default is 3
+    transition: 'slideDown' // What jQuery transition effect you want. Default is slideDown
+  });
+
 });
